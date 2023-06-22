3c243fa9c283ddbab9dac709aa08fccc997cb26b
Toerana <116728100+Toerana@users.noreply.github.com>
2023-06-22 16:50:07 +0200

    Add files via upload

---
+++ MQTT/Script BDD.sql
@@ -0,0 +1,22 @@
+DROP DATABASE IF EXISTS mqttdb;
+CREATE DATABASE mqttdb;
+USE mqttdb;
+
+CREATE TABLE Capteur (
+id VARCHAR(50) NOT NULL,
+nom_capteur VARCHAR(50) NOT NULL,
+piece VARCHAR(50) NOT NULL,
+PRIMARY KEY (id)
+);
+
+CREATE TABLE Donnee (
+id varchar(50) NOT NULL,
+date_ date NOT NULL,
+heure time NOT NULL,
+temperature float NOT NULL,
+CONSTRAINT fk_Capteur
+	FOREIGN KEY (id)
+	REFERENCES Capteur(id)
+);
+
+INSERT INTO Capteur (id,nom_capteur,piece) VALUES ('1025','Capteur1','Salon')
