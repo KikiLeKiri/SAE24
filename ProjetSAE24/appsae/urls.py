185735ef5dc2c0372a4c928b932a351bb4b7df8e
KikiLeKiri <116727828+KikiLeKiri@users.noreply.github.com>
2023-06-27 14:51:06 +0200

    Jaj

--- ProjetSAE24/appsae/urls.py
+++ ProjetSAE24/appsae/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path('capteur_list/', views.capteur_list, name='capteur_list'),
     path('donnee_list/<int:capteur_id>/', views.donnee_list, name='donnee_list'),
     path('modifier_capteur/<int:capteur_id>/', views.modifier_capteur, name='modifier_capteur'),
+    path('toute_donnee/', views.toute_donnee, name='toute_donnee'),
 ]
--- ProjetSAE24/appsae/views.py
+++ ProjetSAE24/appsae/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.views import View
 from .models import *
 from .forms import *
+from datetime import datetime
+
 
 # Create your views here.
 
@@ -13,7 +15,6 @@ def donnee_list(request, capteur_id):
     capteur = Capteur.objects.get(id=capteur_id)
     donnees = Donnee.objects.filter(id_capteur=capteur)
 
-    # Filtrer les données en fonction des paramètres de filtre
     start_date = request.GET.get('start_date')
     end_date = request.GET.get('end_date')
     start_time = request.GET.get('start_time')
@@ -37,7 +38,6 @@ def donnee_list(request, capteur_id):
 
 def modifier_capteur(request, capteur_id):
     capteur = Capteur.objects.get(id=capteur_id)
-
     if request.method == 'POST':
         form = CapteurForm(request.POST, instance=capteur)
         if form.is_valid():
@@ -45,5 +45,18 @@ def modifier_capteur(request, capteur_id):
             return redirect('donnee_list', capteur_id=capteur_id)
     else:
         form = CapteurForm(instance=capteur)
-
     return render(request, 'appsae/modifier_capteur.html', {'form': form, 'capteur_id': capteur_id})
+
+def toute_donnee(request):
+    datatest = []
+    capteurs = Capteur.objects.all()
+    donnees = Donnee.objects.all()
+    for capteur in capteurs:
+        data = Donnee.objects.filter(id_capteur = capteur)
+        datatest.append([capteur, data] )
+    context = {
+        'capteurs': capteurs,
+        'donnees': donnees,
+        'dataset' : datatest                
+    }
+    return render(request, 'appsae/toute_donnee.html', context)
